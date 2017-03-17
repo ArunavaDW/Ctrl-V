60322a6bcba2a4dcfb28b1694c757ffb2e102045
AnAndroid <discusswitharunava@gmail.com>
2017-03-17 08:32:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,33 @@
 var create_account_btn = document.getElementById('create_account_form_submit');
+var create_paste_btn = document.getElementById('create_paste_submit');
+
 console.log(create_account_btn);
+create_paste_btn.onclick = function() {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+
+    if(request.readystate === XMLHttpRequest.DONE){
+      if(request.status === 200){
+          alert("Paste Created Successfully!");
+      } else {
+        alert("Some Internal Error Occured!\nPlease Try Again Later!");
+      }
+    }
+  };
+
+  var pasteBody = document.getElementById('main_paste').value;
+  var pasteTitle = document.getElementById('paste_title').value;
+  var pasteAuthor = document.getElementById('paste_as').value;
+  var pasteTime = new Date();
+
+  request.open('POST', 'http://arunavadw.imad.hasura-app.io/create_paste', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({PasteBody: pasteBody,
+                              PasteTitle: pasteTitle,
+                              PasteAuthor: pasteAuthor
+                              PasteTime:   pasteTime}));
+};
+
 create_account_btn.onclick = function() {
 
   var request = new XMLHttpRequest();
@@ -30,4 +58,4 @@ create_account_btn.onclick = function() {
                               lastname: lastName,
                               email: mailAddr,
                               password: password}));
-}
+};
