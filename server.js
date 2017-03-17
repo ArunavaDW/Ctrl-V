4902c83e314418f60de6b6f856d2f7fc783372d0
AnAndroid <discusswitharunava@gmail.com>
2017-03-17 09:08:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'arunavadw',
@@ -11,6 +13,78 @@ var config = {
     port: '5432',
     password: process.env.DB_PASSWORD
 };
+
+
+function createProfileTemplate() {
+    var profileTemplate = `
+    
+    <!DOCTYPE html>
+    <html lang="en-US">
+    
+    <head>
+      <!-- Page Colors
+            #9143c8 -> Purple
+            #06a209 -> Green
+          -->
+      <title>Ctrl+V</title>
+      <link rel="shortcut icon" type="image/gif/png" href="favicon.ico" />
+    
+      <meta charset="utf-8">
+      <meta name="description" content="A place where one could paste documents and
+      access it from any where in the web">
+      <meta name="keywords" content="ctrl, v, paste, clipboard, online">
+      <meta name="author" content="Arunava Chakraborty">
+      <meta name="viewport" content="width=device-width initial-scale=2.0">
+    
+      <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body class="the_body">
+    
+      <div id="theNavigationBar">
+        <ul>
+          <li><a class="navBarOption_site_name" href=''>Ctrl+V</a></li>
+          <br/>
+          <li class="navBarOptions"><a href="">Main</a></li>
+          <li class="navBarOptions"><a href="">New Paste</a></li>
+          <li class="navBarOptions"><a href="">Edit Profile</a></li>
+          <li class="navBarOptions"><a href="">Browse</a></li>
+        </ul>
+      </div>
+    
+      <div class="center_wrap">
+        <div id="identifier_main">
+          <div class="identifier_1">
+            <img id="theProfilePicture" src="blank-profile-picture.png" alt="Profile Picture"
+            width="130" height="130" class="profile_picture" />
+          </div>
+          <div class="identifier_1">
+            <div>
+              <h1 id="theFName">Arunava</h1>
+            </div>
+            <div>
+              <p id="theUserBio">This User has no Bio</p>
+            </div>
+          </div>
+        </div>
+        <div id="identifier_next">
+          <div>
+            <h2 id="ctrlvHitsHeader">Ctrl+V Hits:</h2><span id="ctrlvHits">0<span>
+          </div>
+          <div id="identifier_show_ctrlv">
+            <div>
+              <h2>Ctrl+V by you:</h2>
+            </div>
+            <div id="ctrlvhitsbox">
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+    
+    </html>
+    `;
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -30,12 +104,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
 var pool = new Pool(config);
@@ -45,9 +115,46 @@ var pool = new Pool(config);
   res.sendFile(path.join(__dirname, 'theProfile.html'));
 });*/
 
-app.get('/ui/ctrlVIcon.png', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'ctrlVIcon.png'));
+app.post('/login', function(req, res){
+    
+    var userName = req.body.username;
+    var passWord = req.body.password;
+    
+    pool.query('SELECT * FROM ctrlvusers WHERE username = $1', [userName], function(err, res) {
+        
+        if (err) {
+          res.status(500).send(err.toString());
+        } else {
+              if (result.rows.length === 0) {
+                  res.status(403).send('username/password is invalid');
+              } else {
+                  // Match the password
+                  var dbString = result.rows[0].password;
+                  var salt = dbString.split('$')[2];
+                  var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+                  if (hashedPassword === dbString) {
+                    
+                    // Set the session
+                    req.session.auth = {userId: result.rows[0].id};
+                    // set cookie with a session id
+                    // internally, on the server side, it maps the session id to an object
+                    // { auth: {userId }}
+                    
+                    res.send('credentials correct!');
+                    
+                  } else {
+                    res.status(403).send('username/password is invalid');
+        }
+              }
+        }
 });
+});
+
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
 
 app.post('/create_account', function(req, res){
 
@@ -55,7 +162,10 @@ app.post('/create_account', function(req, res){
   var firstName = req.body.firstname;
   var lastName = req.body.lastname;
   var email = req.body.email;
-  var password = req.body.password;
+  var unEncryptedPassword = req.body.password;
+  
+  var salt = crypto.randomBytes(128).toString('hex');
+  var password = hash(password, salt);
 
   pool.query('INSERT INTO "ctrlvusers" (username, email, firstname, lastname, password) VALUES ($1, $2, $3, $4, $5)', 
             [username, email, firstName, lastName, password], 
