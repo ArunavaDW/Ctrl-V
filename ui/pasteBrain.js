de7a0863026bb2f74cff6da6ca5abdff25411fc4
AnAndroid <discusswitharunava@gmail.com>
2017-03-19 09:01:46 +0530

    [imad-console] Updates ui/pasteBrain.js

--- ui/pasteBrain.js
+++ ui/pasteBrain.js
@@ -18,6 +18,7 @@ create_paste_btn.onclick = function() {
   var pasteBody = document.getElementById('main_paste').value;
   var pasteTitle = document.getElementById('paste_title').value;
   var pasteAuthor = document.getElementById('paste_as').value;
+  console.log(pasteTitle);
   var pasteTime = new Date();
   
   console.log(pasteTime);
