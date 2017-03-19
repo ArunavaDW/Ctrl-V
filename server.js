2f1cf422e1d2475264c6043b317d6a8de9c37b76
AnAndroid <discusswitharunava@gmail.com>
2017-03-19 10:30:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,6 +103,7 @@ function createPasteTemplate(pasteData){
     var body = pasteData.paste_body;
     var link = pasteData.paste_link;
     var title = pasteData.paste_title;
+    console.log(body);
     
     var pasteTemplate = `
     <!DOCTYPE html>
