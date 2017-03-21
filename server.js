94e5bc712e409efa04f3ce612fa7d71aa95c5d59
AnAndroid <discusswitharunava@gmail.com>
2017-03-21 12:58:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var pool = new Pool(config);
 app.use(function(req, res) {
     if (req.session && req.session.auth && req.session.auth.userId) {
        // Load the user object
-       GLOBAL.LoggedIn = true;
+       LoggedIn = true;
        console.log("LoggedIn");
    }
 });
