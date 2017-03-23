8a58eda2503d838ed8d37faa1884e85877b6741a
AnAndroid <discusswitharunava@gmail.com>
2017-03-23 16:22:28 +0530

    Create profileEdit.js

---
+++ ui/profileEdit.js
@@ -0,0 +1,6 @@
+var theDp = document.getElementById('theProfilePicture');
+var theDpLink = document.getElementById('dpLink');
+function loadTheImage(){
+  console.log("dfdsfsdfsdf");
+  theDp.src = theDpLink.value;
+}
